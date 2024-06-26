acb4b9a7beec4d3a5dfd583bececcaef7bf9a0f0
trienilu3000 <trienilu3000@gmail.com>
2024-06-26 18:04:21 +0700

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/header.component';
-import { FooterComponent } from './common/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductDetailComponent } from './pages/home/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
